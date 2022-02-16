6519e4115a994019c1f7716b91dd734aec9e2f53
James Murphy <jamesm8228@gmail.com>
2022-02-16 13:49:17 -0500

    Pair Programming WordSearch

--- wordsearch.js
+++ wordsearch.js
@@ -1,25 +1,25 @@
 const wordSearch = (letters, word) => {
-    const horizontalJoin = letters.map(ls => ls.join(''));
-    for (let l of horizontalJoin) {
-      if (l.includes(word)) {
-        return true;
-      } else {
-        for (let o = 0; o < letters[0].length; o++) {
-          let tempArr = [];
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) {
+      return true;
+    } else {
+      for (let o = 0; o < letters[0].length; o++) {
+        let tempArr = [];
           
-          for (let i = 0; i < letters.length; i++) {
-            tempArr.push(letters[i][o]);
-          }
-          tempArr = tempArr.join('');
-         
-          if (tempArr.includes(word)) {
-            return true;
-          }
+        for (let i = 0; i < letters.length; i++) {
+          tempArr.push(letters[i][o]);
         }
-        
+        tempArr = tempArr.join('');
+         
+        if (tempArr.includes(word)) {
+          return true;
+        }
       }
+        
     }
-    return false;
-  };
+  }
+  return false;
+};
 
-module.exports = wordSearch
+module.exports = wordSearch;
